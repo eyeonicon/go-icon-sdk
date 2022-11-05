4e37c4190d4fb0c7b83f90dfc5001f4715030c84
Paul Rouge <paulrouge@outlook.com>
2022-11-05 11:30:27 +0100

    .

--- src/main.go
+++ src/main.go
@@ -4,8 +4,10 @@ import (
 	"github.com/icon-project/goloop/client"
 	"fmt"
 	"paulrouge/go-icon-sdk/networks"
-	// "paulrouge/go-icon-sdk/wallet"
+	"paulrouge/go-icon-sdk/wallet"
 	// "github.com/icon-project/goloop/server/jsonrpc"
+	"paulrouge/go-icon-sdk/transactions"
+	"paulrouge/go-icon-sdk/util"
 
 )
 
@@ -20,7 +22,25 @@ func main() {
 	Client := client.NewClientV3(networks.GetActiveNetwork().URL)
 	_ = Client
 
-	fmt.Println()
-	
-	txobject := transactions.TransferICXBuilder("hx9c13cd371aed69c79870b3a3f7492c10122f0315", "1000000000000000000")
+	Wallet := wallet.LoadWallet("../mywallets/keystore.json", "joejoe")
+
+
+	bn := util.ICXToLoop(1.5)
+
+	// fmt.Println(bn)
+
+	// fmt.Printf("type of bn: %T\n", bn)
+		
+	txobject := transactions.TransferICXBuilder("hx9c13cd371aed69c79870b3a3f7492c10122f0315", bn)
+
+	tx, err := Client.SendTransaction(Wallet, txobject)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(tx)
+
+
+
+	// _ = txobject
 }
